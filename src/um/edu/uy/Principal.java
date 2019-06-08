d93125f0a2d0c1c7a0e04384acb875f9b627cfc3
Jose  Pedro Algorta <jpalgort@gmail.com>
2019-06-08 20:04:15 -0300

    'lodelospuntocoma'

--- src/um/edu/uy/Principal.java
+++ src/um/edu/uy/Principal.java
@@ -1,5 +1,7 @@
 package um.edu.uy;
 
+import um.edu.uy.Tads.Hash.ClosedHash;
+import um.edu.uy.Tads.Hash.ColissionManagement;
 import um.edu.uy.Tads.KeyNotFoundException;
 import um.edu.uy.Tads.Hash.OpenHash;
 import um.edu.uy.Tads.TreeJope.BTree;
@@ -20,13 +22,9 @@ public class Principal {
 
         System.out.println("Cargando datos...");
 
-        Athlete[] atletas = new Athlete[140000];
-
-        OpenHash<String, NationalOlympicCommitte> regions = new OpenHash<>(231, 232);
+        ClosedHash<String, NationalOlympicCommitte> regions = new ClosedHash<>(232, 232, ColissionManagement.LINEAR);
 
         OpenHash<Integer, AthleteOlympicParticipation> participations = new OpenHash<>(140000, 140000);
-
-        BTree<String, Team> teams = new BTree<>();
 
         int i = 0;
 
@@ -48,9 +46,13 @@ public class Principal {
 
                 String[] nocs = line1.split(",");
 
-                regions.put(nocs[0], new NationalOlympicCommitte(nocs[1], nocs[2]));
+                if (nocs.length == 3) {
 
-                teams.insert(nocs[0], new Team(nocs[1]));
+                    nocs[2] = nocs[2].substring(1, nocs[2].length() - 1);
+
+                    regions.put(nocs[0], new NationalOlympicCommitte(nocs[0], nocs[1], nocs[2]));
+
+                } else regions.put(nocs[0], new NationalOlympicCommitte(nocs[0], nocs[1], null));
 
                 i++;
 
@@ -69,13 +71,20 @@ public class Principal {
 
             while ((line2 = br2.readLine()) != null) {
 
-                String[] atleta = line2.split(",");
+                String[] atleta = line2.split(";");
+
+                //atleta[0] = atleta[0].substring(1, atleta[0].length() - 1);
 
                 long id = Long.parseLong(atleta[0]);
+
+                //atleta[1] = atleta[1].substring(1, atleta[1].length() - 1);
+
 
                 String name = atleta[1];
 
                 SexType sex;
+
+                //atleta[2] = atleta[2].substring(1, atleta[2].length() - 1);
 
                 if (atleta[2].equals("M")) {
 
@@ -87,18 +96,33 @@ public class Principal {
 
                 }
 
-                int age = Integer.parseInt(atleta[3]);
+                int age;
 
-                float height = Float.parseFloat(atleta[4]);
+                if (!atleta[3].equals("NA")) age = Integer.parseInt(atleta[3]);
+                else age = 0;
 
-                float weight = Float.parseFloat(atleta[5]);
+                float height;
 
-                atletas[i] = new Athlete(id, name, sex, age, height, weight,
-                        teams.find(atleta[6]), regions.get(atleta[7]));
+                if (!atleta[4].equals("NA")) height = Float.parseFloat(atleta[4]);
+                else height = 0;
+
+                float weight;
+
+                if (!atleta[5].equals("NA")) weight = Float.parseFloat(atleta[5]);
+                else weight = 0;
+
+                //atleta[7] = atleta[7].substring(1, atleta[7].length() - 1);
+
+                Team team = new Team(regions.get(atleta[7]).getRegion());
+
+                Athlete atletas = new Athlete(id, name, sex, age, height, weight,
+                        team, regions.get(atleta[7]));
 
                 SeasonType season;
 
-                if (atleta[11].equals("Winter")) {
+                //atleta[10] = atleta[10].substring(1, atleta[10].length() - 1);
+
+                if (atleta[10].equals("Winter")) {
 
                     season = SeasonType.WINTER;
 
@@ -108,24 +132,34 @@ public class Principal {
 
                 }
 
-                City city = new City(atleta[12]);
+                //atleta[11] = atleta[11].substring(1, atleta[11].length() - 1);
 
-                OlympicGame game = new OlympicGame(atleta[9], Integer.parseInt(atleta[10]), season, city);
+                City city = new City(atleta[11]);
 
-                Sport sport = new Sport(atleta[13]);
+                //atleta[8] = atleta[8].substring(1, atleta[8].length() - 1);
 
-                Event event = new Event(atleta[14], sport);
+                OlympicGame game = new OlympicGame(atleta[8], Integer.parseInt(atleta[9]), season, city);
+
+                //atleta[12] = atleta[12].substring(1, atleta[12].length() - 1);
+
+                Sport sport = new Sport(atleta[12]);
+
+                //atleta[13] = atleta[13].substring(1, atleta[13].length() - 1);
+
+                Event event = new Event(atleta[13], sport);
+
+                //atleta[14] = atleta[14].substring(1, atleta[14].length() - 1);
 
                 MedalType medal;
 
-                switch (atleta[15]) {
+                switch (atleta[14]) {
                     case "Gold":
                         medal = MedalType.GOLD;
                         break;
                     case "Silver":
                         medal = MedalType.SILVER;
                         break;
-                    case "Brozne":
+                    case "Bronze":
                         medal = MedalType.BRONZE;
                         break;
                     default:
@@ -133,12 +167,12 @@ public class Principal {
                         break;
                 }
 
-                participations.put(Integer.parseInt(atleta[0]), new AthleteOlympicParticipation(medal, atletas[i], game));
+                participations.put(Integer.parseInt(atleta[0]), new AthleteOlympicParticipation(medal, atletas, game));
 
                 i++;
 
             }
-        } catch (IOException | KeyNotFoundException | EmptyTreeException e) {
+        } catch (IOException | KeyNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -153,7 +187,6 @@ public class Principal {
             switch (sc.nextInt()) {
 
                 case 1:
-
 
 
                     break;
