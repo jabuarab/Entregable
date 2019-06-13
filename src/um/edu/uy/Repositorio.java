40741ae00a5eed94feebbb440cf839c347b863cc
Jose  Pedro Algorta <jpalgort@gmail.com>
2019-06-13 20:34:08 -0300

    'normalizando_datos'

--- src/um/edu/uy/Repositorio.java
+++ src/um/edu/uy/Repositorio.java
@@ -56,11 +56,16 @@ public class Repositorio {
 
                 if (nocs.length == 3) {
 
-                    nocs[2] = nocs[2].substring(1, nocs[2].length() - 1);
+                    if (nocs[0].equals("SIN")) nocs[0] = "SGP";
 
                     regions.put(nocs[0], new NationalOlympicCommitte(nocs[0], nocs[1], nocs[2]));
 
-                } else regions.put(nocs[0], new NationalOlympicCommitte(nocs[0], nocs[1], null));
+                } else {
+
+                    if (nocs[0].equals("SIN")) nocs[0] = "SGP";
+
+                    regions.put(nocs[0], new NationalOlympicCommitte(nocs[0], nocs[1], null));
+                }
 
 
             }
@@ -75,20 +80,19 @@ public class Repositorio {
 
             while ((line2 = br2.readLine()) != null) {
 
-                String[] atleta = line2.split(",");
+                String[] atleta = line2.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
 
-                //atleta[0] = atleta[0].substring(1, atleta[0].length() - 1);
+                atleta[0] = atleta[0].substring(1, atleta[0].length() - 1);
 
                 long id = Long.parseLong(atleta[0]);
 
-                //atleta[1] = atleta[1].substring(1, atleta[1].length() - 1);
-
+                atleta[1] = atleta[1].substring(1, atleta[1].length() - 1);
 
                 String name = atleta[1];
 
-                SexType sex;
+                atleta[2] = atleta[2].substring(1, atleta[2].length() - 1);
 
-                //atleta[2] = atleta[2].substring(1, atleta[2].length() - 1);
+                SexType sex;
 
                 if (atleta[2].equals("M")) {
 
@@ -115,7 +119,7 @@ public class Repositorio {
                 if (!atleta[5].equals("NA")) weight = Float.parseFloat(atleta[5]);
                 else weight = 0;
 
-                //atleta[7] = atleta[7].substring(1, atleta[7].length() - 1);
+                atleta[7] = atleta[7].substring(1, atleta[7].length() - 1);
 
                 Team team = new Team(regions.get(atleta[7]).getRegion());
 
@@ -125,7 +129,7 @@ public class Repositorio {
 
                 SeasonType season;
 
-                //atleta[10] = atleta[10].substring(1, atleta[10].length() - 1);
+                atleta[10] = atleta[10].substring(1, atleta[10].length() - 1);
 
                 if (atleta[10].equals("Winter")) {
 
@@ -137,23 +141,23 @@ public class Repositorio {
 
                 }
 
-                //atleta[11] = atleta[11].substring(1, atleta[11].length() - 1);
+                atleta[11] = atleta[11].substring(1, atleta[11].length() - 1);
 
                 City city = new City(atleta[11]);
 
-                //atleta[8] = atleta[8].substring(1, atleta[8].length() - 1);
+                atleta[8] = atleta[8].substring(1, atleta[8].length() - 1);
 
                 OlympicGame game = new OlympicGame(atleta[8], Integer.parseInt(atleta[9]), season, city);
 
-                //atleta[12] = atleta[12].substring(1, atleta[12].length() - 1);
+                atleta[12] = atleta[12].substring(1, atleta[12].length() - 1);
 
                 Sport sport = new Sport(atleta[12]);
 
-                //atleta[13] = atleta[13].substring(1, atleta[13].length() - 1);
+                atleta[13] = atleta[13].substring(1, atleta[13].length() - 1);
 
                 Event event = new Event(atleta[13], sport);
 
-                //atleta[14] = atleta[14].substring(1, atleta[14].length() - 1);
+                atleta[14] = atleta[14].substring(1, atleta[14].length() - 1);
 
                 MedalType medal;
 
